fd34adc80d3c9ede0a3f0fd31cbc42a7f6303eb5
christian-supy-io <christian@supy.io>
2026-03-18 13:04:53 +0200

    fix: update API resource identifiers to include domain in environment configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
       authority: 'https://login.microsoftonline.com/1eda1ee1-2c51-412e-8654-4901e72243c7',
       redirectUri: 'https://masq-board.netlify.app',
     },
-    apiScopes: ['api://7028812e-7502-40a0-9aac-4e860bf07bcf/access_as_user'],
+    apiScopes: ['api://masq-board.netlify.app/7028812e-7502-40a0-9aac-4e860bf07bcf/access_as_user'],
     graphScopes: ['Calendars.Read', 'OnlineMeetings.ReadWrite', 'User.Read'],
   },
   teamsAppId: '7028812e-7502-40a0-9aac-4e860bf07bcf',
